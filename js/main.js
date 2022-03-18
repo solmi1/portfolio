db91717dc2c63d76b22da79f24594225ecca0f0c
solmi1 <some763@naver.com>
2022-03-18 11:59:12 +0900

    포트폴리오 첫번째

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const btnCall = document.querySelector(".btnCall");
+
+const menuMo = document.querySelector(".menuMo");
+
+btnCall.onclick = function(e){
+    e.preventDefault();
+
+    btnCall.classList.toggle("on");
+
+    menuMo.classList.toggle("on");
+}
