e19ee7eecd2dc8bccdbe4bb1e2c38da7118ecfa8
solmi1 <some763@naver.com>
2022-03-31 00:01:19 +0900

    popup 서비스페이지 수정

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 const btnCall = document.querySelector(".btnCall");
 const menuMo = document.querySelector(".menuMo");
 
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    spaceBetween: 20,
-});
+const popup = document.querySelector("#popup");
+const btnClose = popup.querySelector(".close");
+const isCookie = document.cookie.indexOf("today=done");
+let isOn;
 
 btnCall.onclick = function(e){
     e.preventDefault();
@@ -15,7 +14,34 @@ btnCall.onclick = function(e){
     menuMo.classList.toggle("on");
 }
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+});
 
+
+if(isCookie == -1){
+    popup.style.display = "block";
+}else{
+    popup.style.display = "none";
+}
+
+btnClose.addEventListener("click", e=>{
+    e.preventDefault();
+    popup.style.display = "none";
+
+    let isChecked = popup.querySelector("input[type=checkbox]").checked;
+    if(isChecked) setCookie("today", "done", 1);
+})
+
+function setCookie(name, val, due){
+    const today = new Date();
+    const day = today.getDate();
+    today.setDate(day + due);
+    const duedate = today.toGMTString();
+
+    document.cookie = `${name}=${val}; path=/; expires=${duedate}`;
+}
 
 
 
