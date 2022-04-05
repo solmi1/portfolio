d4a1ac511d07b9fd0cb869e7b4d9305ac1cf0895
solmi1 <some763@naver.com>
2022-04-06 00:45:24 +0900

    board 레이아웃 변경-자바 미적용

--- js/services.js
+++ js/services.js
@@ -1,37 +1,17 @@
-/*
-// 서비스 페이지에 들어왔을 때 특정 스크롤 위치에서 박스가 차례대로 나오게
-const btn = document.querySelector(".btn");
-const box1 = document.querySelector(".box1");
-const box2 = document.querySelector(".box2");
-const img1 = box1.querySelector(".img");
-const img2 = box2.querySelector(".img");
+const tab = document.querySelector("#plan");
+const btns = tab.querySelectorAll(".btns li a");
+const boxs = tab.querySelectorAll("div");
 
-btn.addEventListener("click", ()=>{
-    new Anim(box1,{
-        prop:"left",
-        value:0,
-        duration:700,
-        callback:()=>{
-            new Anim(img1,{
-                prop:"left",
-                value:826,
-                duration:700,
-                callback:()=>{
-                    new Anim(box2,{
-                        prop:"left",
-                        value:354,
-                        duration:700,
-                        callback:()=>{
-                            new Anim(img2,{
-                                prop:"left",
-                                value:-354,
-                                duration:700,
-                            })
-                        }
-                    })
-                }
-            })
-        }
+btns.forEach(function(btn, index){
+    btn.addEventListener("click", e=>{
+        activation(btns, index);
+        activation(boxs, index);
     })
-})
-*/
+});
+
+function activation(arr, index){
+    for(let el of arr){
+        el.classList.remove("on");
+    }
+    arr[index].classList.add("on");
+}
